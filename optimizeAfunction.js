02370e3ccc6275e6c497ecfbdee87295921f3179
Shravan Shetty <shravan.shetty@colorado.edu>
2019-02-05 01:50:03 -0700

    Added more details to the optimize function

--- optimizeAfunction.js
+++ optimizeAfunction.js
@@ -1,49 +1,52 @@
-// could be potentially more than 3 keys in the object above
-items = [
+let items = [
     {color: 'red', type: 'tv', age: 18},
     {color: 'silver', type: 'phone', age: 20}
 
 ]
 
-excludes = [
+let  excludes = [
     {k: 'color', v: 'silver'},
-    {k: 'type', v: 'tv1'},
+    {k: 'type', v: 'tv'},
 ]
-
-// function excludeItems(items, excludes) {
-//     excludes.forEach(pair => {
-//         items = items.filter(item => {
-//             //Main condition
-//             //1. check if the key is there if its not there then dont exclued
-//             //2. and if not equal then add it back to the items may be next one will exclude them (Live another day to fight)
-//             if (item[pair.k] && item[pair.k]!== pair.v) {
-//                 return item
-//             }
-//         });
-//         console.log(items)
-//     });
-//     return items;
-// }
-
-
+f
 function excludeItems(items, excludes) {
-    return items.filter((item) => {
-        return excludes.filter((exclusion) => {
-            return item[exclusion.k] === exclusion.v
-        });
-})
+    let items_new = []
+    excludes.forEach(pair => {
+        items_new = items.filter(item => item[pair.k] === pair.v);
+    });
+    return items_new;
 }
-console.log(excludeItems(items, excludes))
 
-// 1. Describe what this function is doing...
-// 2. What is wrong with that function ?
-// // 3. How would you optimize it ?
-//
-//
-// arr = [1, 2, 2, 4, 5, 6]
-//
-// console.log(arr.filter(item => {
-//     if (item < 5) {
-//         return item
-//     }
-// }))
+// now shoould I output excluded items??
+
+function excludeExcludedItems(items, exludes) {
+    let excluded_items = []
+    excluded_items = items.filter(function(item) {
+        return excludes.some(function(pair){
+            return  item[pair.k] === pair.v;
+        })
+    })
+    return excluded_items;
+
+}
+
+
+
+
+// now should I output remainung elements in the item?
+
+
+function excludeRemianingItems(items, exludes) {
+    let remaining_items = []
+    remaining_items = items.filter(function(item) {
+        return excludes.every(function(pair){
+            return  item[pair.k] !== pair.v;
+        })
+    })
+    return remaining_items;
+
+}
+console.log(excludeItems(items,excludes));
+
+console.log(excludeExcludedItems(items,excludes));
+console.log(excludeRemianingItems(items,excludes));
