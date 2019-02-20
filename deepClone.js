66706cf6eb9c1c9ce44f7ff5542bed6f713e0ed1
Shravan Shetty <shravan.shetty@colorado.edu>
2019-02-20 03:02:38 -0700

    Updated the final deep cloning method

--- deepClone.js
+++ deepClone.js
@@ -1,55 +1,66 @@
-//Its all about deep cloning now
-var assert = require('assert')
-/*
-It is understood form these checks is that new array will have problems too while copying
-function copyArray(arr) {
-  let final = []
-  final.push(...arr)
-  return final
-}
-
-let arr1 =  [1,2,23, function () {} ,  {name:"shravan"}]
-let copiedArray =copyArray(arr1);
-
-assert(copiedArray[3] !== arr1[3], "array funciton failed")
-assert(copiedArray[4] !== arr1[4], "object funciton failed")
-*/
+// Back to deep cloning
+//1. All the primitive types should be added
+//2. functions check
+//3, Date object check
+//4. Array
+const assert = require('assert')
 
 function clone(obj) {
-    let final = Object.prototype.toString.call(obj) === "[object Object]" ? {} : (Object.prototype.toString.call(obj) === "[object Array]"?[] :{})
-    for (let key of Object.keys(obj)) {
-        let value = obj[key];
-        if(Object.prototype.toString.call(value) === "[object Object]" ||Object.prototype.toString.call(value) === "[object Array]") {
-            final[key] = clone(value)
-        }else if(Object.prototype.toString.call(value) === "[object Function]") {
-            final[key] = value.bind(final);
-        }else {
-            final[key] = value;
+
+
+    let finalObj = {}
+    //Array check
+    finalObj = Array.isArray(obj) ? [] : {}
+
+    //Date check
+    // if (obj instanceof Date) return new Date(obj.getTime())
+
+    if (obj.constructor == Date || obj.constructor == RegExp || obj.constructor == String || obj.constructor == Number || obj.constructor == Boolean) {
+        return new obj.constructor(obj);
+    }
+    for (let prop in obj) {
+        if (isObject(obj[prop])) {
+            finalObj[prop] = clone(obj[prop])
+        }
+        else if (typeof obj[prop] === "function") {
+            finalObj[prop] = obj[prop].bind(finalObj)
+        }
+        else
+        {
+            finalObj[prop] = obj[prop]
         }
 
-
     }
-    return final;
+
+    return finalObj
+
+
+}
+
+function isObject(item) {
+    if (item !== null && typeof item === "object") return true
 
 }
 
 
 let obj1 = {
-    name :"Shravan",
-    age : 26,
+    name: "Shravan",
+    age: 26,
 
-    getAge : function() {return this.age},
-    address : {
-        city : 'Boulder',
-        state : "Colorado",
-        name : {
-            "add" :"2747"
+    getAge: function () {
+        return this.age
+    },
+    address: {
+        city: 'Boulder',
+        state: "Colorado",
+        name: {
+            "add": "2747"
 
         }
 
     },
 
-    classes : ['bigData', 'Algo']
+    classes: ['bigData', 'Algo']
 
 }
 
@@ -64,63 +75,3 @@ assert(cloned.getAge !== obj1.getAge, "function equlity failed")
 
 
 assert(cloned.classes !== obj1.classes, "array equlity failed")
-console.log(cloned.getAge() , obj1.getAge(), )
-
-
-
-
-
-/*
-function extend(from, to) {
-  if (from == null || typeof from != "object") return from;
-
-  if (from.constructor != Object && from.constructor != Array) return from;
-
-  if (from.constructor == Date || from.constructor == RegExp || from.constructor == Function ||
-    from.constructor == String || from.constructor == Number || from.constructor == Boolean)
-    return new from.constructor(from);
-
-  to = to || new from.constructor();
-
-  for (var name in from) {
-    to[name] = typeof to[name] == "undefined" ? extend(from[name], null) : to[name];
-  }
-
-  return to;
-}
-
-var obj = {
-  date: new Date(),
-  func: function(q) {
-    return 1 + q;
-  },
-  num: 123,
-  text: "asdasd",
-  array: [1, "asd"],
-  regex: new RegExp(/aaa/i),
-  subobj: {
-    num: 234,
-    text: "asdsaD"
-  }
-}
-
-var clone = extend(obj);
-
-/* log objects to DOM */
-var origNode = document.getElementById("orig");
-origNode.innerHTML = JSON.stringify(obj);
-
-var cloneNode = document.getElementById("clone")
-cloneNode.innerHTML = JSON.stringify(clone);
-
-
-//test dates
-setTimeout(function() {
-    obj.date = new Date();
-    origNode.innerHTML += "<br/>" + JSON.stringify(obj);
-    cloneNode.innerHTML += "<br/>" + JSON.stringify(clone);
-}, 1500)
-
-
-
- */
